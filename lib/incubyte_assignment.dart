512c3732fc6e6e10f36860be60cebd22a3508e12
Shravanti <shravanti@MadhavaRajus-MacBook-Air.local>
2025-01-28 16:14:28 +0530

    Added code snippet to handle the (1,2)=3 test case scenario

--- lib/incubyte_assignment.dart
+++ lib/incubyte_assignment.dart
@@ -8,6 +8,12 @@ class StringCalculator {
       return int.tryParse(numbers)!;
     }
 
-    return int.tryParse(numbers)!;
+    List<String> listOfNumbers = numbers.split(delimiter);
+    int sum = 0;
+    for (int i = 0; i < listOfNumbers.length; i++) {
+      sum += int.tryParse(listOfNumbers[i])!;
+    }
+
+    return sum;
   }
 }
