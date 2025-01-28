6de71aea3ad072ce231232b7f1486ee3d39bf01d
Shravanti <shravanti@MadhavaRajus-MacBook-Air.local>
2025-01-28 16:08:23 +0530

    Handled the actual code for the single input failed test case

--- lib/incubyte_assignment.dart
+++ lib/incubyte_assignment.dart
@@ -2,8 +2,12 @@ class StringCalculator {
   int addNumbers(String numbers) {
     if (numbers.isEmpty) {
       return 0;
-    } else {
-      return 0;
     }
+    String delimiter = ',';
+    if (!numbers.contains(delimiter) && numbers.isNotEmpty) {
+      return int.tryParse(numbers)!;
+    }
+
+    return int.tryParse(numbers)!;
   }
 }
