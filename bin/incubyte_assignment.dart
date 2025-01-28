bcd9ebbfae357146dd4d3052758e79f0c327849f
Shravanti <shravanti@MadhavaRajus-MacBook-Air.local>
2025-01-28 15:43:24 +0530

    First Commit

---
+++ bin/incubyte_assignment.dart
@@ -0,0 +1,5 @@
+import 'package:incubyte_assignment/incubyte_assignment.dart' as incubyte_assignment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${incubyte_assignment.calculate()}!');
+}
---
+++ test/incubyte_assignment_test.dart
@@ -0,0 +1,8 @@
+import 'package:incubyte_assignment/incubyte_assignment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
