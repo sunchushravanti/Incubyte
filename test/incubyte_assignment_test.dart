9a068ff1a7d17e6a22fa72d740392852ef2615ad
Shravanti <shravanti@MadhavaRajus-MacBook-Air.local>
2025-01-28 15:56:34 +0530

    Initial test for the requirement

--- test/incubyte_assignment_test.dart
+++ test/incubyte_assignment_test.dart
@@ -2,7 +2,11 @@ import 'package:incubyte_assignment/incubyte_assignment.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  int add(String numbers) {
+    return 0; // Minimal implementation to start failing tests
+  }
+
+  test('Function should return 0 for an empty string', () {
+    expect(add(""), 0);
   });
 }
